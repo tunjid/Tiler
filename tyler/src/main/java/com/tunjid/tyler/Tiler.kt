67ceb96ac2bbeb1cb0e31112911efe2ce320449d
Adetunji Dahunsi <dahunsi@google.com>
2021-07-11 10:37:58 -0400

    Amortize sorting cost

--- tyler/src/main/java/com/tunjid/tyler/Tiler.kt
+++ tyler/src/main/java/com/tunjid/tyler/Tiler.kt
@@ -13,16 +13,20 @@ internal data class Tiler<Query, Item>(
     fun add(output: Tile.Output<Query, Item>): Tiler<Query, Item> = when (output) {
         is Tile.Output.Data -> copy(
             shouldEmit = true,
+            // Only sort queries when they output the first time to amortize the cost of sorting.
+            order = when {
+                queryToTiles.contains(output.query) -> order
+                else -> order.updateQueries(
+                    order.metadata.sortedQueries
+                        .plus(output.query)
+                        .distinct()
+                        .sortedWith(order.comparator)
+                )
+            },
             queryToTiles = queryToTiles.apply { put(output.query, output.tile) }
         )
         is Tile.Output.Started -> copy(
             shouldEmit = false,
-            order = order.updateQueries(
-                order.metadata.sortedQueries
-                    .plus(output.query)
-                    .distinct()
-                    .sortedWith(order.comparator)
-            )
         )
         is Tile.Output.Eviction -> copy(
             shouldEmit = true,
@@ -41,38 +45,38 @@ internal data class Tiler<Query, Item>(
 internal fun <Query, Item> Tile.Order<Query, Item>.flatten(
     queryToTiles: Map<Query, Tile<Query, Item>>
 ): List<Item> {
+    val sortedQueries = metadata.sortedQueries
+
     return when (val order = this) {
         is Tile.Order.Unspecified -> queryToTiles.keys
             .fold(mutableListOf()) { list, query ->
                 list.add(element = queryToTiles.getValue(query).item)
                 list
             }
-        is Tile.Order.Sorted -> queryToTiles.keys
-            .sortedWith(order.comparator)
+        is Tile.Order.Sorted -> sortedQueries
             .foldWhile(mutableListOf(), order.limiter) { list, query ->
                 list.add(element = queryToTiles.getValue(query).item)
                 list
             }
         is Tile.Order.PivotSorted -> {
-            val sorted = order.metadata.sortedQueries
 
             // TODO: Amortize this as well.
             val mostRecentQuery: Query = queryToTiles.keys
                 .maxByOrNull { queryToTiles.getValue(it).flowOnAt }
                 ?: return emptyList()
 
-            val startIndex = sorted.indexOf(mostRecentQuery)
+            val startIndex = sortedQueries.indexOf(mostRecentQuery)
             var leftIndex = startIndex
             var rightIndex = startIndex
-            val result = mutableListOf(queryToTiles.getValue(sorted[startIndex]).item)
+            val result = mutableListOf(queryToTiles.getValue(sortedQueries[startIndex]).item)
 
-            while (!order.limiter(result) && (leftIndex >= 0 || rightIndex <= sorted.lastIndex)) {
+            while (!order.limiter(result) && (leftIndex >= 0 || rightIndex <= sortedQueries.lastIndex)) {
                 if (--leftIndex >= 0) result.add(
                     index = 0,
-                    element = queryToTiles.getValue(sorted[leftIndex]).item
+                    element = queryToTiles.getValue(sortedQueries[leftIndex]).item
                 )
-                if (++rightIndex <= sorted.lastIndex) result.add(
-                    element = queryToTiles.getValue(sorted[rightIndex]).item
+                if (++rightIndex <= sortedQueries.lastIndex) result.add(
+                    element = queryToTiles.getValue(sortedQueries[rightIndex]).item
                 )
             }
             result
