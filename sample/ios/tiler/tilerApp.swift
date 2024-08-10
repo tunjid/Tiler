3ca2303bceb55ca4fb1970db0109c387e9f9946b
Adetunji Dahunsi <tjdah100@gmail.com>
2024-08-10 11:21:21 -0400

    Add ios app files

---
+++ sample/ios/tiler/tilerApp.swift
@@ -0,0 +1,17 @@
+//
+//  tilerApp.swift
+//  tiler
+//
+//  Created by adetunji_dahunsi on 8/9/24.
+//
+
+import SwiftUI
+
+@main
+struct tilerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
